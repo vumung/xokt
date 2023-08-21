7f7dc534cea25532a0668b830b5a0715c1a8505f
vumung <73631542+vumung@users.noreply.github.com>
2023-08-21 05:18:45 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 # 定义 UUID 及伪装路径、哪吒面板参数，请自行修改. (注意:伪装路径以 / 符号开始,为避免不必要的麻烦,请不要使用特殊符号.)
-UUID='de04add9-5c68-8bab-950c-08cd5320df18'
+UUID='cc26226a-d2bc-42c7-98df-16b9012c94e4'
 VMESS_WSPATH='/vmess'
 VLESS_WSPATH='/vless'
 TROJAN_WSPATH='/trojan'
-SS_WSPATH='/shadowsocks'
+SS_WSPATH='/ss'
 NEZHA_SERVER=''
 NEZHA_PORT=''
 NEZHA_KEY=''
